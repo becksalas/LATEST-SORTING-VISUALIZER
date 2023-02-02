b9520506552a1a85ab7ad4da1dc0f24478405083
someone <ebeck.salas@gmail.com>
2023-02-02 03:55:01 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SortingAlgorithms from './SortingVisualizer/SortingVisualizer';
+import './App.css';  
+
+function App() {
+  return (
+    <div className="App">
+      <SortingAlgorithms></SortingAlgorithms>
+    </div>
+  );
+}
+
+export default App;
